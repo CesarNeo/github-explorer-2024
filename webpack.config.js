240109c3fc59206eebadf0412575ebc670171762
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-04-02 18:46:53 -0300

    refactor: add new plugin to generate index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -20,5 +21,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'public', 'index.html'),
+  })],
 }
